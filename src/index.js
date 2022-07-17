36ccebb935aca09c104dd7a0288a813ca7efc643
Eva <bebinaeva@gmail.com>
2022-07-17 18:13:42 +0300

    feat:  implement function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function toReadable (number) {
-  
+
+    const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve',
+                    'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+
+    const century = Math.floor(number / 100);
+    number = number % 100;
+    const ten = Math.floor(number / 10);
+    const one = Math.floor(number % 10);
+    let res = "";
+    if (century) {
+        res += (((res === "") ? "" : " ") +
+            toReadable(century) +" hundred");
+    }
+    if (ten>0 || one>0)
+        if (!(res===""))
+        {
+            res += " ";
+        }
+
+    {
+        if (ten < 2)
+        {
+            res += ones[ten * 10 + one];
+        }
+        else
+        {
+
+            res += tens[ten];
+            if (one>0)
+
+            {
+                res += (" " + ones[one]);
+            }
+        }
+    }
+    if (res === "")
+    {
+        res = "zero";
+    }
+    return res;
 }
